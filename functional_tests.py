1e36b09b5b46f924e43834fc46862561bf2a97c3
YvonTre <yvongforever@gmail.com>
2017-08-25 13:57:01 +0800

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -39,16 +39,17 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly"(Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to mae a fly"(Edith is very methodical)
-
         # The page updates again, and now shows both items on her list. Then she sees
         # that the site has generated a unique URL for her - there is some
         # explanatory text to that effect.
